7325e7af1c71b783143d6a2e9961347cdb0d0f0a
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2023-01-09 14:13:52 -0500

    created db, schema, and seeds files

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE election;
+
+USE election;
+
+-- to add to database, go to mysql cli and type source *path of file*
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Stephon', 'Treadwell', 1),
+  ('Qualicia', 'Quiles', 1),
+  ('Treymane', 'Lewis', 0),
+  ('Tashad', 'Jordan', 1),
+  ('Airika', 'Hurling', 1),
+  ('Jahhad', 'Crawley', 0),
+  ('Abriel', 'Culver', 0),
+  ('Myron', 'Jordan', 1),
+  ('Lebron', 'James', 1),
+  ('Kevin', 'Durant', 1);
