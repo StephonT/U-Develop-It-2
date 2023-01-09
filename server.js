76d7b0087d22e8d6230372b3bb6ea11bdc09eb2e
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2023-01-09 18:00:55 -0500

    started database calls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,13 +8,28 @@ const app = express();
 app.use(express.urlencoded({ exteded: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //MySQL username
+    user: "root",
+    //MySQL password
+    password: "LeahBean2020!",
+    database: "election",
+  },
+  console.log(`Connected to the election database🗄`)
+);
+
+//Database Calls
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
-
-
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}✨✔`);
